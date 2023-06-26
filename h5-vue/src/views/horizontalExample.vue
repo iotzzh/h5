0d89e3834ba7cf4fe020065c14fff22cf3ae95dc
zzh <956159241@qq.com>
2023-06-26 22:11:49 +0800

    添加基础页面

--- h5-vue/src/views/horizontalExample.vue
+++ h5-vue/src/views/horizontalExample.vue
@@ -2,13 +2,17 @@
 <template>
   <section v-horizontal-screen @touchmove.prevent class="box">
     <!-- 页面具体内容 -->
-    Hello World!
-    <button @click="clickIntoHome">点击进入主页</button>
+    <!-- Hello World!
+    <button @click="clickIntoHome">点击进入主页</button> -->
+
+    <img :src="IndexButtonImg" class="image" @click="clickIntoHome" />
+
   </section>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import IndexButtonImg from '../assets/button.png'
 
 const router = useRouter();
 
@@ -21,10 +25,19 @@ const clickIntoHome = () => {
 
 <style scoped>
 .box {
-  background: url(../assets/bg.jpg) no-repeat;
-  background-size: cover;
+  /* background: url(../assets/bg.jpg) no-repeat; */
+  /* background-size: cover; */
   width: 100%;
   height: 100%;
   text-align: center;
+  background-color: rgb(207,163,100);
+  position: relative;
+}
+
+.image {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
--- h5-vue/src/views/horizontalExampleMain.vue
+++ h5-vue/src/views/horizontalExampleMain.vue
@@ -3,11 +3,15 @@
 <template>
   <section v-horizontal-screen>
     <swiper :modules="modules" class="mySwiper">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG1})` }"><img class="img-btn" :src="Icon1" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG2})` }"><img class="img-btn" :src="Icon2" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG3})` }"><img class="img-btn" :src="Icon3" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG4})` }"><img class="img-btn" :src="Icon4" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG5})` }"><img class="img-btn" :src="Icon5" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG6})` }"><img class="img-btn" :src="Icon6" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG7})` }"><img class="img-btn" :src="Icon7" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG8})` }"><img class="img-btn" :src="Icon8" /></div></swiper-slide>
+    <swiper-slide><div class="box" :style="{ backgroundImage: `url(${BG9})` }"><img class="img-btn" :src="Icon9" /></div></swiper-slide>
   </swiper>
   </section>
  
@@ -18,6 +22,24 @@
 import { ref } from 'vue';
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import BG1 from '../assets/01.png';
+import BG2 from '../assets/02.png';
+import BG3 from '../assets/03.png';
+import BG4 from '../assets/04.png';
+import BG5 from '../assets/05.png';
+import BG6 from '../assets/06.png';
+import BG7 from '../assets/07.png';
+import BG8 from '../assets/08.png';
+import BG9 from '../assets/09.png';
+import Icon1 from '../assets/icon01.png';
+import Icon2 from '../assets/icon02.png';
+import Icon3 from '../assets/icon03.png';
+import Icon4 from '../assets/icon04.png';
+import Icon5 from '../assets/icon05.png';
+import Icon6 from '../assets/icon06.png';
+import Icon7 from '../assets/icon07.png';
+import Icon8 from '../assets/icon08.png';
+import Icon9 from '../assets/icon09.png';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -54,14 +76,14 @@ const modules =  [Pagination];
   align-items: center;
 }
 
-.swiper-slide img {
+/* .swiper-slide img {
   display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
-}
+} */
 
-.swiper-pagination-bullet {
+/* .swiper-pagination-bullet {
   width: 20px;
   height: 20px;
   text-align: center;
@@ -75,6 +97,24 @@ const modules =  [Pagination];
 .swiper-pagination-bullet-active {
   color: #fff;
   background: #007aff;
+} */
+
+.box {
+  width: 100%;
+  height: 100%;
+  background-repeat: repeat;
+  background-size: cover;
+  /* object-fit: cover; */
+  position: relative;
+}
+
+.img-btn {
+  width: 150px;
+  height: auto;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 90px;
 }
 
 </style>
